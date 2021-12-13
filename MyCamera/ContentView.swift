d6413cb03d7754ec304fc8f806fa1494179d771a
rickmeta <rickmeta45@gmail.com>
2021-12-13 21:28:13 +0900

    Initial Commit

---
+++ MyCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCamera
+//
+//  Created by rickmeta on 2021/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by rickmeta on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
